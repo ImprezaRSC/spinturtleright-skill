7a3eebfe87d50dcc6aa442d06594c284b5ffe1ac
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-19 13:11:56 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+import subprocess
 
 
 class Spinturtleright(MycroftSkill):
@@ -8,7 +9,8 @@ class Spinturtleright(MycroftSkill):
     @intent_file_handler('spinturtleright.intent')
     def handle_spinturtleright(self, message):
         self.speak_dialog('spinturtleright')
-
+        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, -2.0, 0.0]' '[2.0, 0.0, 0.0]'"
+        subprocess.call([s],shell=True)
 
 def create_skill():
     return Spinturtleright()
